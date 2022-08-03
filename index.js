74cfae960910972f92f03c13057b959bf41b0792
npngSteph <stephane.ahmedboulanger@gmail.com>
2022-08-03 18:02:13 +0200

    first

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app,get("/", (req, res) => {
-    res.send("Hello World");
-    }
-
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000");
-    };
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    });
